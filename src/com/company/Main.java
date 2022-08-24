1a834311a768a3ef7cbe81cbf478f5fd4b1be7b0
azast <petteri.hokka@icloud.com>
2022-08-24 15:45:37 +0300

    Only exercises 8 & 10

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,33 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	int tracker = 1;
-    int a = 4;
-    int b = 3;
-    int n = 3;
-    int x = 1;
+
+
+        //homework 10
+        int a = 6;
+        int b = 5;
+        int solution;
+        int r = a % b;
+        boolean isFound = false;
+
+
+        //homework 10
+        while(isFound == false){
+            if(a % b == 0) {
+                solution = b;
+                isFound = true;
+                System.out.println(solution);
+            }
+            else
+                a = b;
+                b = r;
+        }
+
+        //int tracker = 1;
+    //int a = 4;
+    //int b = 3;
+    //int n = 3;
+    //int x = 1;
 
     //while(tracker<a){
         //n = n * 3;
@@ -15,10 +37,10 @@ public class Main {
     //}
         //System.out.println(n);
 
-    while(tracker<=a){
-        x = x * b;
-        tracker = tracker + 1;
-    }
-        System.out.println(x);
+    //while(tracker<=a){
+      //  x = x * b;
+        //tracker = tracker + 1;
+    //}
+      //  System.out.println(x);
     }
 }
