e4418a02d68a1b410f6d7a59147e293480ec8af7
azast <petteri.hokka@icloud.com>
2022-08-24 13:50:13 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int tracker = 1;
+    int a = 4;
+    int b = 3;
+    int n = 3;
+    int x = 1;
+
+    //while(tracker<a){
+        //n = n * 3;
+        //tracker = tracker + 1;
+    //}
+        //System.out.println(n);
+
+    while(tracker<=a){
+        x = x * b;
+        tracker = tracker + 1;
+    }
+        System.out.println(x);
+    }
+}
